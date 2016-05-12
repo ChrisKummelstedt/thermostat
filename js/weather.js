7448f97be3d28b27ebbf38178fa641d6e30fb8e6
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-05-12 16:49:31 +0200

    api is great, make sure you live in one of 4 cities

--- js/weather.js
+++
@@ -1,8 +0,0 @@
-
-
-$('#current-city').change(function() {
-  var city = $('#current-city').val();
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=8baf71f983a57fb4649ec5f123fc4b34&units=metric', function(data) {
-    $('#weather_temperature').text(data.main.temp);
-  });
-  })
